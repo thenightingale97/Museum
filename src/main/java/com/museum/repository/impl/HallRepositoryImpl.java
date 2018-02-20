772fed633fec808355594a5a9668aff859e6cf89
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-20 13:12:18 +0200

    Added Hall and Showpiece repository implementation.

---
+++ src/main/java/com/museum/repository/impl/HallRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.museum.repository.impl;
+
+
+import com.museum.entity.Hall;
+import com.museum.repository.AbstractRepository;
+import com.museum.repository.HallRepository;
+
+public class HallRepositoryImpl extends AbstractRepository<Hall, Integer> implements HallRepository {
+
+}
---
+++ src/main/java/com/museum/repository/impl/ShowpieceRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.museum.repository.impl;
+
+
+import com.museum.entity.Showpiece;
+import com.museum.repository.AbstractRepository;
+import com.museum.repository.ShowpieceRepository;
+
+public class ShowpieceRepositoryImpl extends AbstractRepository<Showpiece, Integer> implements ShowpieceRepository {
+
+}
