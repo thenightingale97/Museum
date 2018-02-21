83a2cd1e1542e051dfc25b391e41ecdf9301c24f
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-21 11:16:51 +0200

    Added query for task 2.

--- src/main/java/com/museum/repository/ShowpieceRepository.java
+++ src/main/java/com/museum/repository/ShowpieceRepository.java
@@ -2,7 +2,10 @@ package com.museum.repository;
 
 import com.museum.entity.Showpiece;
 
+import java.util.List;
 
 
 public interface ShowpieceRepository extends Repository<Showpiece, Integer> {
+    
+    List<Showpiece> getByAuthor(int authorId);
 }
--- src/main/java/com/museum/repository/impl/ShowpieceRepositoryImpl.java
+++ src/main/java/com/museum/repository/impl/ShowpieceRepositoryImpl.java
@@ -5,6 +5,22 @@ import com.museum.entity.Showpiece;
 import com.museum.repository.AbstractRepository;
 import com.museum.repository.ShowpieceRepository;
 
-public class ShowpieceRepositoryImpl extends AbstractRepository<Showpiece, Integer> implements ShowpieceRepository {
+import javax.persistence.TypedQuery;
+import java.util.List;
 
+public class ShowpieceRepositoryImpl extends AbstractRepository<Showpiece, Integer> implements ShowpieceRepository {
+    
+    /**
+     * Task 2.
+     *
+     * @param authorId - id of some Author
+     * @return list of showpieces belongs to some author
+     */
+    @Override
+    public List<Showpiece> getByAuthor(int authorId) {
+        String sql = "SELECT showpiece FROM Showpiece showpiece WHERE showpiece.author.id = ?1";
+        TypedQuery<Showpiece> query = getEntityManager().createQuery(sql, Showpiece.class);
+        query.setParameter(1, authorId);
+        return query.getResultList();
+    }
 }
