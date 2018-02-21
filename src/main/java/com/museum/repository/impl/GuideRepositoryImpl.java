30c3b5b808fdae5d262e5e0ca0c8d376425a72e8
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-21 20:41:11 +0200

    Added query for task 6.

--- src/main/java/com/museum/repository/impl/GuideRepositoryImpl.java
+++ src/main/java/com/museum/repository/impl/GuideRepositoryImpl.java
@@ -1,14 +1,14 @@
 package com.museum.repository.impl;
 
 
-import java.util.List;
-
-import javax.persistence.TypedQuery;
-
 import com.museum.entity.Guide;
 import com.museum.entity.GuidePosition;
 import com.museum.repository.AbstractRepository;
 import com.museum.repository.GuideRepository;
+
+import javax.persistence.TypedQuery;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class GuideRepositoryImpl extends AbstractRepository<Guide, Integer> implements GuideRepository {
     
@@ -23,5 +23,23 @@ public class GuideRepositoryImpl extends AbstractRepository<Guide, Integer> impl
         TypedQuery<Guide> query = getEntityManager().createQuery(sql, Guide.class);
         query.setParameter(1, position);
         return query.getResultList();
+    }
+    
+    /**
+     * Task 6.
+     */
+    public List<Guide> getByPeriod(LocalDateTime fromTime, LocalDateTime toTime) {
+        String sql = "SELECT guide FROM Guide guide " +
+                "WHERE guide NOT IN (" +
+                "SELECT guide FROM Guide JOIN guide.events event WHERE " +
+                "(event.startTime <= :fromTime AND event.finishTime > :fromTime)" +
+                "OR (event.startTime < :toTime AND event.finishTime >= :toTime)\n" +
+                "OR (event.startTime >= :fromTime AND event.finishTime <= :toTime))";
+        TypedQuery<Guide> query = getEntityManager().createQuery(sql, Guide.class);
+        query.setParameter("fromTime", fromTime);
+        query.setParameter("toTime", toTime);
+        
+        List<Guide> resultList = query.getResultList();
+        return resultList;
     }
 }
