b5aad03225b9cb282e38d31c1bb247839a81f855
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-22 10:40:05 +0200

    Added queries for task 8.

--- src/main/java/com/museum/repository/ShowpieceRepository.java
+++ src/main/java/com/museum/repository/ShowpieceRepository.java
@@ -1,8 +1,11 @@
 package com.museum.repository;
 
-import java.util.List;
-
 import com.museum.entity.Showpiece;
+import com.museum.entity.ShowpieceMaterial;
+import com.museum.entity.ShowpieceTechnique;
+
+import java.util.List;
+import java.util.Map;
 
 public interface ShowpieceRepository extends Repository<Showpiece, Integer> {
     
@@ -11,4 +14,8 @@ public interface ShowpieceRepository extends Repository<Showpiece, Integer> {
     List<Showpiece> getByAuthor(int authorId);
 
     List<Showpiece> getByGuardian(int guardianId);
+    
+    Map<ShowpieceTechnique, Long> getStatisticByTechnique();
+    
+    Map<ShowpieceMaterial, Long> getStatisticByMaterial();
 }
