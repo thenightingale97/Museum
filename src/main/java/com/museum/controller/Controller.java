0a21e7f54af0a9b9de5ff90cb4874537fcd4c617
Yura Katola <yurakatola@gmail.com>
2018-02-22 16:41:16 +0200

    Add jdbc.properties file

--- src/main/java/com/museum/controller/Controller.java
+++ src/main/java/com/museum/controller/Controller.java
@@ -1,32 +1,9 @@
 package com.museum.controller;
 
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import org.hibernate.SessionFactory;
-
-import com.museum.entity.Author;
-import com.museum.entity.Event;
-import com.museum.entity.Guide;
-import com.museum.entity.GuidePosition;
-import com.museum.entity.Showpiece;
-import com.museum.repository.EventRepository;
-import com.museum.repository.GuideRepository;
-import com.museum.repository.ShowpieceRepository;
-import com.museum.repository.impl.EventRepositoryImpl;
-import com.museum.repository.impl.GuideRepositoryImpl;
-import com.museum.repository.impl.ShowpieceRepositoryImpl;
-import com.museum.service.AuthorService;
-import com.museum.service.GuideService;
-import com.museum.service.ShowpieceService;
-import com.museum.service.impl.AuthorServiceImpl;
-import com.museum.service.impl.GuideServiceImpl;
-import com.museum.service.impl.ShowpieceServiceImpl;
-
 
 
 public class Controller {
