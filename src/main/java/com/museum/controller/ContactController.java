bad1977e8712834d28c9d90baf06b290d5c7f5b9
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-23 09:18:32 +0200

    Updated index.jsp and ContactController.

--- src/main/java/com/museum/controller/ContactController.java
+++ src/main/java/com/museum/controller/ContactController.java
@@ -1,15 +1,21 @@
 package com.museum.controller;
 
 
+import com.museum.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactController {
     
+    @Autowired
+    AuthorService authorService;
+    
     @RequestMapping("/")
-    public String home() {
-        System.out.println("fjksdfjskldfjsldfjdslfj");
+    public String home(Model model) {
+        model.addAttribute("authorName", authorService.find(1).getLastName());
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -11,6 +11,7 @@
     <title>Title</title>
 </head>
 <body>
+<p>${authorName}</p>
 <p>Hello</p>
 </body>
 </html>
