c82836f5a6b709aa46d0b0a2151ddb40044e1515
Yura Katola <yurakatola@gmail.com>
2018-02-27 19:57:24 +0200

    Added PeriodRequest.

---
+++ src/main/java/com/museum/model/request/PeriodRequest.java
@@ -0,0 +1,24 @@
+package com.museum.model.request;
+
+public class PeriodRequest {
+
+    private String fromDateTime;
+
+    private String toDateTime;
+
+    public String getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(String fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public String getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(String toDateTime) {
+        this.toDateTime = toDateTime;
+    }
+}
