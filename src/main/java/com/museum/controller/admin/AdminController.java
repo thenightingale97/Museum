89995835bc1edcf197ebae00383fb860dda61867
Yura Katola <yurakatola@gmail.com>
2018-02-28 12:18:51 +0200

    Moved AuthorController and AdminCOntroller to directory admin.

--- src/main/java/com/museum/controller/admin/AdminController.java
+++ src/main/java/com/museum/controller/admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.museum.controller;
+package com.museum.controller.admin;
 
 import com.museum.model.view.AuthorView;
 import com.museum.service.AuthorService;
@@ -8,34 +8,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/admin/author")
-public class AuthorController {
+@RequestMapping("/admin")
+public class AdminController {
 
     @Autowired
     private AuthorService authorService;
 
     @GetMapping
-    public String author(Model model) {
+    public String admin(Model model) {
         model.addAttribute("authors", authorService.findAll());
-        return "admin-author";
+        return "admin";
     }
 
     @PostMapping
     public String addAuthor(@ModelAttribute("authorView") AuthorView authorView) {
         authorService.save(authorView);
-        return "redirect:/admin/author";
+        return "redirect:/admin";
     }
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable int id){
         authorService.delete(id);
-        return "redirect:/admin/author";
+        return "redirect:/admin";
     }
 
     @RequestMapping("/update/{id}")
     public String update(@PathVariable int id, Model model){
         model.addAttribute("authorView", authorService.find(id));
-        return "admin-author";
+        return "admin";
     }
 
     @ModelAttribute("authorView")
--- src/main/java/com/museum/controller/admin/AuthorController.java
+++ src/main/java/com/museum/controller/admin/AuthorController.java
@@ -1,5 +1,6 @@
-package com.museum.controller;
+package com.museum.controller.admin;
 
+import com.museum.entity.Author;
 import com.museum.model.view.AuthorView;
 import com.museum.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,38 +9,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/admin")
-public class AdminController {
+@RequestMapping("/admin/author")
+public class AuthorController {
 
     @Autowired
     private AuthorService authorService;
 
     @GetMapping
-    public String admin(Model model) {
+    public String author(Model model) {
         model.addAttribute("authors", authorService.findAll());
-        return "admin";
+        return "admin-author";
     }
 
     @PostMapping
-    public String addAuthor(@ModelAttribute("authorView") AuthorView authorView) {
+    public String addAuthor(@ModelAttribute("authorView") Author authorView) {
         authorService.save(authorView);
-        return "redirect:/admin";
+        return "redirect:/admin/author";
     }
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable int id){
         authorService.delete(id);
-        return "redirect:/admin";
+        return "redirect:/admin/author";
     }
 
     @RequestMapping("/update/{id}")
     public String update(@PathVariable int id, Model model){
         model.addAttribute("authorView", authorService.find(id));
-        return "admin";
+        return "admin-author";
     }
 
     @ModelAttribute("authorView")
-    public AuthorView getForm() {
-        return new AuthorView();
+    public Author getForm() {
+        return new Author();
     }
 }
