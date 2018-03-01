2348e7bf5ff12ef5cdd69da52223acebedcf03aa
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-01 18:06:01 +0200

    Updated EventFilter with FilterUtil.

--- src/main/java/com/museum/model/filter/EventFilter.java
+++ src/main/java/com/museum/model/filter/EventFilter.java
@@ -1,58 +1,33 @@
 package com.museum.model.filter;
 
+import com.museum.model.filter.util.FilterUtil;
 import com.museum.model.request.EventFilterRequest;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class EventFilter {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
-    private final Integer id;
+    private final Integer excurtionId;
 
     private final LocalDateTime fromDateTime;
 
     private final LocalDateTime toDateTime;
 
-    public EventFilter(Integer id, LocalDateTime fromDateTime, LocalDateTime toDateTime) {
-        this.id = id;
+    public EventFilter(Integer excurtionId, LocalDateTime fromDateTime, LocalDateTime toDateTime) {
+        this.excurtionId = excurtionId;
         this.fromDateTime = fromDateTime;
         this.toDateTime = toDateTime;
     }
 
     public static EventFilter of(EventFilterRequest request) {
         return new EventFilter(
-                parseExcursionId(request.getExcursionId()),
-                parseDateTime(request.getFromDateTime()),
-                parseDateTime(request.getToDateTime()));
+                FilterUtil.parseExcursionId(request.getExcursionId()),
+                FilterUtil.parseDateTime(request.getFromDateTime()),
+                FilterUtil.parseDateTime(request.getToDateTime()));
     }
 
-    private static LocalDateTime parseDateTime(String dateTime) {
-        if (dateTime == null) {
-            return null;
-        }
-        try {
-            return LocalDateTime.parse(dateTime, DATE_TIME_FORMATTER);
-        } catch (DateTimeParseException ex) {
-            return null;
-        }
-    }
-
-    private static Integer parseExcursionId(String excursionId) {
-        if (excursionId == null) {
-            return null;
-        }
-        try {
-            return Integer.valueOf(excursionId);
-        } catch (NumberFormatException ex) {
-            return null;
-        }
-    }
-
-    public Integer getId() {
-        return id;
+    public Integer getExcurtionId() {
+        return excurtionId;
     }
 
     public LocalDateTime getFromDateTime() {
@@ -61,5 +36,17 @@ public class EventFilter {
 
     public LocalDateTime getToDateTime() {
         return toDateTime;
+    }
+
+    public boolean hasExcursionId() {
+        return excurtionId != null;
+    }
+
+    public boolean hasFromDateTime() {
+        return fromDateTime != null;
+    }
+
+    public boolean hasToDateTime() {
+        return toDateTime != null;
     }
 }
