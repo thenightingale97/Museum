9d246f88f15a2347310f4afb2f3ddfe140e3dcab
Yura Katola <yurakatola@gmail.com>
2018-03-01 14:59:12 +0200

    Added ShowpieceController.

---
+++ src/main/java/com/museum/controller/ShowpieceController.java
@@ -0,0 +1,50 @@
+package com.museum.controller;
+
+import com.museum.entity.Showpiece;
+import com.museum.model.filter.ShowpieceFilter;
+import com.museum.model.request.ShowpieceFilterRequest;
+import com.museum.service.AuthorService;
+import com.museum.service.HallService;
+import com.museum.service.ShowpieceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ShowpieceController {
+
+    @Autowired
+    private ShowpieceService showpieceService;
+
+    @Autowired
+    private AuthorService authorService;
+
+    @Autowired
+    private HallService hallService;
+
+    @GetMapping("/showpiece")
+    public String showpiece(Model model, @RequestParam("id") int id) {
+        model.addAttribute("showpiece", showpieceService.find(id));
+        return "showpiece";
+    }
+
+    @GetMapping("/showpieces")
+    public String showPieces(Model model, @ModelAttribute("showpieceFilterRequest")
+            ShowpieceFilterRequest showpieceFilterRequest) {
+        List<Showpiece> showpieces = showpieceService.findAllByFilter(ShowpieceFilter.of(showpieceFilterRequest));
+        model.addAttribute("showpieces", showpieces);
+        model.addAttribute("authors", authorService.findAll());
+        model.addAttribute("halls", hallService.findAll());
+        return "showpieces";
+    }
+
+    @ModelAttribute("showpieceFilterRequest")
+    public ShowpieceFilterRequest populateShowpieceFilterRequest() {
+        return new ShowpieceFilterRequest();
+    }
+}
